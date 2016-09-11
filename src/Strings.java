f2aa418a0ccf0aea94c4dc034ce80176d4009bdd
bcdennis@gmail.com <bcdennis@gmail.com>
2016-09-11 16:05:54 -0500

    adding in Strings.java for Lecture 03

---
+++ src/Strings.java
@@ -0,0 +1,18 @@
+/*
+ * Strings.java
+ *
+ * This is a simple program that will be used to demonstrate
+ * Strings.
+ *
+ * @author Brad Dennis
+ * Created:  09/11/2016
+ * Modified: --
+ */
+
+public class Strings {
+
+    public static void main(String[] args) {
+        
+    }
+
+}
