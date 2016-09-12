6d68e7c2be2d91caf87bfdc2f4357782ec1e1d5b
dennis <dennis@msoe.edu>
2016-09-12 10:58:38 -0500

    Extended Strings workshop

--- src/Strings.java
+++ src/Strings.java
@@ -68,7 +68,7 @@ public class Strings {
         //Part 6.
         // 1.  Comment the previous block using single line comment syntax.
         // 2.  Uncomment the block below.
-        // 3.  Read about this substring command:  https://docs.oracle.com/javase/8/docs/api/java/lang/String.html#substring-int-int-
+        // 3.  Read about this substring method:  https://docs.oracle.com/javase/8/docs/api/java/lang/String.html#substring-int-int-
         // 4.  Answer the first Part 6 question.
         // 5.  Run this program.
         // 6.  Answer the second Part 6 question.
@@ -80,9 +80,9 @@ public class Strings {
         */
 
         //Part 7.
-        // 1.  Comment the previous block using single line comment syntax.
+        // 1.  Comment the previous block using multi line comment syntax.
         // 2.  Uncomment the block below.
-        // 3.  Read about this substring command:  https://docs.oracle.com/javase/8/docs/api/java/lang/String.html#substring-int-
+        // 3.  Read about this substring method:  https://docs.oracle.com/javase/8/docs/api/java/lang/String.html#substring-int-
         // 4.  Answer the first Part 7 question.
         // 5.  Run this program.
         // 6.  Answer the second Part 7 question.
@@ -97,7 +97,7 @@ public class Strings {
         //Part 8.
         // 1.  Comment the previous block using single line comment syntax.
         // 2.  Uncomment the block below.
-        // 3.  Read about this substring command:  https://docs.oracle.com/javase/8/docs/api/java/lang/String.html#charAt-int-
+        // 3.  Read about this substring method:  https://docs.oracle.com/javase/8/docs/api/java/lang/String.html#charAt-int-
         // 4.  Answer the first Part 8 question.
         // 5.  Run this program.
         // 6.  Answer the second Part 8 question.
@@ -109,9 +109,9 @@ public class Strings {
         */
 
         //Part 9.
-        // 1.  Comment the previous block using single line comment syntax.
+        // 1.  Comment the previous block using multi line comment syntax.
         // 2.  Uncomment the block below.
-        // 3.  Read about this substring command:  https://docs.oracle.com/javase/8/docs/api/java/lang/String.html#length--
+        // 3.  Read about this substring method:  https://docs.oracle.com/javase/8/docs/api/java/lang/String.html#length--
         // 4.  Answer the first Part 9 question.
         // 5.  Run this program.
         // 6.  Answer the second Part 9 question.
@@ -126,11 +126,30 @@ public class Strings {
 
         //Part 10.
         // 1.  Comment the previous block using single line comment syntax.
-        // 3.  Read about this indexOf command:  https://docs.oracle.com/javase/8/docs/api/java/lang/String.html#indexOf-java.lang.String-
-        // 4.  Write a program that will:
+        // 2.  Read about this indexOf method:  https://docs.oracle.com/javase/8/docs/api/java/lang/String.html#indexOf-java.lang.String-
+        // 3.  Write a program that will:
         //     a.  Declare and initialize a variable named myHelloWorld with the string literal "My Hello World".
         //     b.  Print out to the console the index of the letter H.
-        // 6.  Answer the Part 10 question.
+        // 4.  Answer the Part 10 question.
+
+
+        //Part 11.
+        // 1.  Comment the previous block using multi line comment syntax.
+        // 2.  Write a program that will:
+        //     a.  Declare and initialize a variable with the identifier "firstName".
+        //     b.  Assign to it the string literal of your first name.
+        //     c.  Without using loops, one separate lines print out each letter in your name using the charAt method.
+        // 3.  Answer the Part 11 question.
+
+        //Part 12.
+        // 1.  Using the String class javadoc as a reference, what does the 'replaceLast' method do?
+        // 2.  The javadocs for String are here:  https://docs.oracle.com/javase/8/docs/api/java/lang/String.html
+        // 3.  Answer the Part 12 question.
+
+        //Part 13.
+        // 1.  Using the String class javadoc as a reference, what does the 'replace' method do?
+        // 2.  The javadocs for String are here:  https://docs.oracle.com/javase/8/docs/api/java/lang/String.html
+        // 3.  Answer the Part 13 question.
     }
 
 }
