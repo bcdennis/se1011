3f35c3f74944f244677674bcd4b47acea6188189
bcdennis@gmail.com <bcdennis@gmail.com>
2016-09-18 13:04:28 -0500

    Added Iteration Demo

---
+++ src/Iteration.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+/*
+ * Iteration.java
+ *
+ * This is a simple program that will be used to demonstrate
+ * iteration statements in Java
+ *
+ * @author Brad Dennis
+ * Created:  09/18/2016
+ * Modified: --
+ */
+public class Iteration {
+    public static void main(String[] args) {
+        int x = 100;
+        System.out.println("The value of 'x' is " + x);
+        while(x < 200)
+            x++;
+
+        System.out.println("The value of 'x' is " + x);
+        System.out.println("\n\n");
+
+        Scanner standardIn = new Scanner(System.in);
+        boolean isQuitting = false;
+        while (!isQuitting) {
+            System.out.println("Enter any string ('q' or 'Q' to quit): ");
+            String input = standardIn.nextLine();
+            if (input.equalsIgnoreCase("q")) {
+                isQuitting = true;
+            }
+        }
+
+
+
+    }
+}
