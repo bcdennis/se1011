417ed14c5c847ee8850a95e259c412f64e81edaf
bcdennis@gmail.com <bcdennis@gmail.com>
2016-09-25 09:01:28 -0500

    Nested-ifs workshop ready.

--- src/NestedIfs.java
+++ src/NestedIfs.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /*
  * NestedIfs.java
  *
@@ -10,6 +12,36 @@
 public class NestedIfs {
 
     public static void main(String[] args) {
+        Scanner standardIn = new Scanner(System.in);
+        String input;
+        int age;
+
+        System.out.print("Enter in your age: ");
+        input = standardIn.nextLine();
+        age = Integer.parseInt(input);
+
+        if (age < 13) {
+            if (age > 5) {
+                System.out.println("You are in elementary school.");
+            } else if (age > 3) {
+                System.out.println("You are in pre-K");
+            } else {
+                System.out.println("You are in diapers!");
+            }
+
+        } else if (age < 19) {
+            System.out.println("You are in high school");
+
+        } else {
+            if (age < 65) {
+                System.out.println("You are probably working.");
+            } else {
+                System.out.println("Retired! (Maybe?)");
+                if (age > 71) {
+                    System.out.println("Retired for sure!");
+                }
+            }
+        }
 
     }
 }
