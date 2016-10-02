c89009a875c91b07e37e1e60305313fed12a377a
bcdennis@gmail.com <bcdennis@gmail.com>
2016-10-02 11:21:29 -0500

    Tweaking coding standard demo

--- src/CodingStandard.java
+++ src/CodingStandard.java
@@ -6,21 +6,21 @@ public class CodingStandard {
 
         Scanner si = new Scanner(System.in);
         System.out.print("Enter a number: ");
-        int sz = si.nextInt();
+        int diameter = si.nextInt();
         si.nextLine();
-        if (sz < 1) System.out.println("Cannot make a zero or negative sized square.  Exiting.");
+        if (diameter < 1) System.out.println("Cannot make a zero or negative sized square.  Exiting.");
 
         System.out.print("Filled in? (y/n) ");
         String A = si.nextLine();
 
         if (!(A.equalsIgnoreCase("y") || A.equalsIgnoreCase("n"))) System.out.println("Only 'y' or 'n' are permitted.  Exiting.");
-            for (int i = 0; i < sz; i++)
+            for (int i = 0; i < diameter; i++)
                 System.out.print("*");
                 System.out.println();
 
-        for (int i = 1; i < sz - 1; i++);
+        for (int i = 1; i < diameter - 1; i++);
         System.out.print("*");
-            for (int j = 1; j < sz - 1; j++) {
+            for (int j = 1; j < diameter - 1; j++) {
                 if (A.equalsIgnoreCase("y"))
                 {
                     System.out.print("*");
@@ -30,7 +30,7 @@ public class CodingStandard {
                 }
                 System.out.println("*");
         }
-    for (int counter = 0; counter < sz; counter++) {
+    for (int counter = 0; counter < diameter; counter++) {
         System.out.print("*");
     }
         System.out.println();}}
