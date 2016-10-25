e000269a63f9fab9b1a90d59d129ef89bc3ec4f7
bcdennis@gmail.com <bcdennis@gmail.com>
2016-10-25 11:44:10 -0500

    adding in avp

---
+++ src/edu/msoe/aliens/Alien.java
@@ -0,0 +1,17 @@
+/*
+ * SE1011
+ * Fall 2018
+ * Alien
+ * Name: Brad Dennis
+ * Created: 10/25/2018
+ */
+package edu.msoe.aliens;
+
+/**
+ * This class models an alien for our OO practice exercises.
+ *
+ * @author Brad
+ * @version 2016.10.18
+ */
+public class Alien {
+}
